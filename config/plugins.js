4344ca8891871f6408392adb295a1eb8292fdc76
EVERYTHINGING <mikeparisi@gmail.com>
2022-11-25 11:39:30 -0700

    no vercel plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
-    "vercel-deploy": {
-      enabled: true,
-    },
+    // "vercel-deploy": {
+    //   enabled: true,
+    //   config: {
+    //     deployHook: 'https://api.vercel.com/v1/integrations/deploy/prj_2EUWJyxsNPD7Opn26O7Kp8iD5Zad/yhdQZA5naX',
+    //     apiToken: '2Af046bAEuklaFRuiaItNo2V',
+    //   }
+    // },
   });
